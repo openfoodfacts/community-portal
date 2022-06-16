ad6b6aceb585757f9aac334009b63f013c718fdc
Sumit <mr.sumitkrr@gmail.com>
2022-06-16 18:36:17 +0530

    initializing django with new name(community_portal)

--- community_portal/community_portal/urls.py
+++ community_portal/community_portal/urls.py
@@ -1,4 +1,4 @@
-"""server URL Configuration
+"""community_portal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
