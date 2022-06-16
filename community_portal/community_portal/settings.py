f8d5b3924dcd3cd5acc66c21985e38669f73e165
Sumit <mr.sumitkrr@gmail.com>
2022-06-16 19:24:33 +0530

    putting secret key into .env

--- community_portal/community_portal/settings.py
+++ community_portal/community_portal/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vovc0r9o9*)47(_072uiidg(@p0*9r)1hnzij6b59mwg$5f1#$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
